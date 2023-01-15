a6a82d68da388e02860f38ebf05f9d7472fbe6f3
z013d <shantsev.m@ya.ru>
2023-01-15 15:11:26 +0300

    feat: Add image view on background

--- Dice-Marathon/Dice-Marathon/ViewController.swift
+++ Dice-Marathon/Dice-Marathon/ViewController.swift
@@ -9,13 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let backgroundImage = UIImageView()
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        style()
+        layout()
     }
+}
 
-
+extension ViewController {
+    func style() {
+        // BackgroundImage
+        backgroundImage.translatesAutoresizingMaskIntoConstraints = false
+        backgroundImage.image = UIImage(named: "Background")
+    }
+    
+    func layout() {
+        view.addSubview(backgroundImage)
+        
+        // BackgroundImage
+        NSLayoutConstraint.activate([
+            backgroundImage.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundImage.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            backgroundImage.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: backgroundImage.trailingAnchor),
+        ])
+    }
 }
 
