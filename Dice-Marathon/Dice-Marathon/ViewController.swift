9f7b1b8c99fb7e5dbacfa59ae2ab3295487131a6
z013d <shantsev.m@ya.ru>
2023-01-15 15:06:59 +0300

    Initial commit 2

---
+++ Dice-Marathon/Dice-Marathon/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Dice-Marathon
+//
+//  Created by User on 15.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
